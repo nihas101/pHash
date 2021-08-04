92a373f533d92c7debcac7cecd43af960182a996
Nikita Hasert <nikitahasert@gmail.com>
2021-08-04 19:55:09 +0200

    DCT: Add type hints to avoid boxed math

--- src/de/nihas101/phash/dct.clj
+++ src/de/nihas101/phash/dct.clj
@@ -2,30 +2,30 @@
   (:require
    [de.nihas101.phash.utils :as u]))
 
-; TODO: Remove after done
-(set! *warn-on-reflection* true)
-(set! *unchecked-math* true)
-
-(defonce ^:private inv-sqrt-2 (/ 1 (Math/sqrt 2.0)))
+(defonce ^:private inv-sqrt-2 (double (/ 1 (Math/sqrt 2.0))))
 
 (defn- coefficient
   "Returns the coefficient of element `u` in the DCT calculation.
    See: https://www.math.cuhk.edu.hk/~lmlui/dct.pdf"
-  [^long u]
-  (if (zero? u) inv-sqrt-2 1))
+  ^double [^long u]
+  (if (zero? u) inv-sqrt-2 1.0))
+
+(defonce ^:private dct-indexes
+  (mapv #(u/idx-lin->idx-2d % 32) (range 1024)))
 
 (defn- dct-sum-32x32
   "Calculates the sum X(i,j) of the DCT (specific to a 32x32 matrix),
    according to: https://www.math.cuhk.edu.hk/~lmlui/dct.pdf"
-  ^longs [^longs values ^long i ^long j]
-  (reduce +
-          (for [x (range 32)
-                y (range 32)]
-            (* (get values (u/idx-2d->idx-lin x y 32))
-               (Math/cos (quot (* (inc (* 2 x)) i Math/PI) 64))
-               (Math/cos (quot (* (inc (* 2 y)) j Math/PI) 64))))))
+  ^double [^longs values ^long i ^long j]
+  (transduce
+   (map
+    (fn [[^long x ^long y]]
+      (* ^long (get values (u/idx-2d->idx-lin x y 32))
+         ^double (Math/cos (quot (* (inc (* 2 x)) i Math/PI) 64))
+         ^double (Math/cos (quot (* (inc (* 2 y)) j Math/PI) 64)))))
+   + dct-indexes))
 
-(defonce ^:private scale-factor (/ 1 (Math/sqrt (* 2 32))))
+(defonce ^:private scale-factor (double (/ 1 (Math/sqrt 64))))
 
 ;; This is the slow version of the algorithm,
 ;; first calculating all 32x32 transformations before discarding 75% of them
@@ -36,20 +36,18 @@
   [dct-indexes]
   (fn ^longs [values]
     (mapv (fn [[i j]]
-            (* scale-factor
-               (coefficient i) (coefficient j)
+            (* ^double scale-factor
+               (coefficient i)
+               (coefficient j)
                (dct-sum-32x32 values i j)))
           dct-indexes)))
-
-(defonce ^:private dct-indexes (mapv #(u/idx-lin->idx-2d % 32) (range 1024)))
 
 (defonce discret-cosine-transform-32x32
   (discret-cosine-transform-32x32-fn dct-indexes))
 
 (defonce ^:private reduced-dct-indexes
-  (for [x (range 32)
-        y (range 32)
-        :when (and (< x 8) (< y 8))]
+  (for [x (range 8)
+        y (range 8)]
     [x y]))
 
 (defn reduce-dct-32x32->8x8
