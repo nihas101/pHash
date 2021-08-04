ef437db6053f0ff0f62416e12968a3ce45a6fc4c
Nikita Hasert <nikitahasert@gmail.com>
2021-08-04 19:55:43 +0200

    Remove unused elements

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to phash
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/de/nihas101/phash/core_test.clj
+++ test/de/nihas101/phash/core_test.clj
@@ -109,17 +109,6 @@
      tu/compr
      tu/misc)))
 
-; TODO: Rotate and mirror images when comparing them
-;(deftest image-distance-a-hash-compr-rotd-test
-;  (testing "image-distance a-hash compr rotd test"
-;    (mapv
-;     (fn [idx compressed rotated]
-;       (is (< (image-distance compressed rotated) 1)
-;           (str "Image with id " idx " has a hamming distance larger than 1")))
-;     (range)
-;     tu/compr
-;     tu/rotd)))
-
 (deftest image-distance-a-hash-blur-misc-test
   (testing "image-distance a-hash blur misc test"
     (mapv
@@ -129,29 +118,6 @@
      (range)
      tu/blur
      tu/misc)))
-
-; TODO: Rotate and mirror images when comparing them
-;(deftest image-distance-a-hash-blur-rotd-test
-;  (testing "image-distance a-hash blur rotd test"
-;    (mapv
-;     (fn [idx blurred rotated]
-;       (is (< (image-distance a-hash-fn blurred rotated) 1)
-;           (str "Image with id " idx " has a hamming distance larger than 1")))
-;     (range)
-;     tu/blur
-;     tu/rotd)))
-
-; TODO: Rotate and mirror images when comparing them
-;(deftest image-distance-a-hash-misc-rotd-test
-;  (testing "image-distance a-hash misc rotd test"
-;    (mapv
-;     (fn [idx misclns rotated]
-;       (is (< (image-distance a-hash-fn misclns rotated) 1)
-;           (str "Image with id " idx " has a hamming distance larger than 1")))
-;     (range)
-;     tu/misc
-;     tu/rotd)))
-
 
 ;; dHash
 
@@ -211,17 +177,6 @@
      tu/compr
      tu/misc)))
 
-; TODO: Rotate and mirror images when comparing them
-;(deftest image-distance-d-hash-compr-rotd-test
-;  (testing "image-distance d-hash compr rotd test"
-;    (mapv
-;     (fn [idx compressed rotated]
-;       (is (< (image-distance d-hash-fn compressed rotated) 1)
-;           (str "Image with id " idx " has a hamming distance larger than 1")))
-;     (range)
-;     tu/compr
-;     tu/rotd)))
-
 (deftest image-distance-d-hash-blur-misc-test
   (testing "image-distance d-hash blur misc test"
     (mapv
@@ -231,28 +186,6 @@
      (range)
      tu/blur
      tu/misc)))
-
-; TODO: Rotate and mirror images when comparing them
-;(deftest image-distance-d-hash-blur-rotd-test
-;  (testing "image-distance d-hash blur rotd test"
-;    (mapv
-;     (fn [idx blurred rotated]
-;       (is (< (image-distance d-hash-fn blurred rotated) 1)
-;           (str "Image with id " idx " has a hamming distance larger than 1")))
-;     (range)
-;     tu/blur
-;     tu/rotd)))
-
-; TODO: Rotate and mirror images when comparing them
-;(deftest image-distance-d-hash-misc-rotd-test
-;  (testing "image-distance d-hash misc rotd test"
-;    (mapv
-;     (fn [idx misclns rotated]
-;       (is (< (image-distance d-hash-fn misclns rotated) 1)
-;           (str "Image with id " idx " has a hamming distance larger than 1")))
-;     (range)
-;     tu/misc
-;     tu/rotd)))
 
 ;; pHash
 
@@ -312,17 +245,6 @@
      tu/compr
      tu/misc)))
 
-; TODO: Rotate and mirror images when comparing them
-;(deftest image-distance-p-hash-compr-rotd-test
-;  (testing "image-distance p-hash compr rotd test"
-;    (mapv
-;     (fn [idx compressed rotated]
-;       (is (< (image-distance p-hash-fn compressed rotated) 1)
-;           (str "Image with id " idx " has a hamming distance larger than 1")))
-;     (range)
-;     tu/compr
-;     tu/rotd)))
-
 (deftest image-distance-p-hash-blur-misc-test
   (testing "image-distance p-hash blur misc test"
     (mapv
@@ -332,25 +254,3 @@
      (range)
      tu/blur
      tu/misc)))
-
-; TODO: Rotate and mirror images when comparing them
-;(deftest image-distance-p-hash-blur-rotd-test
-;  (testing "image-distance p-hash blur rotd test"
-;    (mapv
-;     (fn [idx blurred rotated]
-;       (is (< (image-distance p-hash-fn blurred rotated) 1)
-;           (str "Image with id " idx " has a hamming distance larger than 1")))
-;     (range)
-;     tu/blur
-;     tu/rotd)))
-
-; TODO: Rotate and mirror images when comparing them
-;(deftest image-distance-p-hash-misc-rotd-test
-;  (testing "image-distance p-hash misc rotd test"
-;    (mapv
-;     (fn [idx misclns rotated]
-;       (is (< (image-distance p-hash-fn misclns rotated) 1)
-;           (str "Image with id " idx " has a hamming distance larger than 1")))
-;     (range)
-;     tu/misc
-;     tu/rotd)))
